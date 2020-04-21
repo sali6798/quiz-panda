f2bce7e37e037d738349cd3380aca11303aeae90
Zahra Ali Aghazadeh <Zahra@Zahras-MacBook-Pro.local>
2020-04-20 20:11:54 -0700

    added some minor stuff but server is not working

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const path = require("path");
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 
 // Requiring our models for syncing
 const db = require("./app/models");
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static directory
-app.use(express.static("app/public"));
+app.use(express.static("./app/public"));
 
 // Set Handlebars.
 const exphbs = require("express-handlebars");
