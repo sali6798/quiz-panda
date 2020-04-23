9b108bc8b5cd2f25f80af7cef6a67449b6e50d7c
bbtwots <brettbelka@gmail.com>
2020-04-23 14:17:11 -0700

    Quiz Submit button is grabbing score, posting it to QuizUser and rerouting to leaderboard

--- app/public/assets/js/takeQuiz.js
+++ app/public/assets/js/takeQuiz.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function () {
     let quizObj;
 
@@ -13,9 +15,9 @@ $(document).ready(function () {
                 if ($("#accesscodeForm").siblings()[3]) {
                     $("#accesscodeForm").siblings()[3].remove();
                 }
-                
+
                 const errorMsg = $("<p>").text("This access code does not exist!").addClass("formError");
-                
+
                 // append it to the div after the form element
                 $("#takeQuiz").append(errorMsg);
 
@@ -30,10 +32,69 @@ $(document).ready(function () {
         })
     })
 
+    //submitAnswers onclick
+    $("#submitAnswers").on("click", function (event) {
+        let quizId;
+        event.preventDefault();
+        let correctAnswers = 0;
+        let radios = document.querySelectorAll(".answerOption");
 
-    
+        radios.forEach(radio => {
+
+            if (radio.checked && Boolean(radio.dataset.correctanswer)) {
+                correctAnswers += 1
+            }
+        });
+        console.log("correct anwers: " + correctAnswers);
+
+        quizId = $(this).data("quizid");
+
+        console.log("quizID: " + quizId);
+
+        $.ajax({
+            method: "GET",
+            url: "/api/quizuser/" + quizId,
+            dataType: "json"
+        }).then(quizUser => {
+            console.log("Get result", quizUser);
+
+            if (quizUser === null) {
+                $.ajax({
+                    method: "POST",
+                    url: "/api/quizuser",
+                    data: {
+                        QuizId: quizId,
+                        hasTaken: true,
+                        score: correctAnswers
+                    }
+                }).then(newQuizUser => {
+                    console.log("NewQuizUser: ", newQuizUser);
+                    console.log("/leaderboard/" + quizId);
+
+                    location.href = "/leaderboard/" + quizId
+
+                })
+            } else {
+                $.ajax({
+                    method: "PUT",
+                    url: "/api/quizuser/" + quizId,
+                    data: {
+                        hasTaken: true,
+                        score: correctAnswers
+                    }
+                }).then(updatedQuizUser => {
+                    console.log("updatedQuizUser: ", updatedQuizUser);
+                    console.log("/leaderboard/" + quizId);
+
+                    location.href = "/leaderboard/" + quizId
+                })
+            }
+
+        })
+
+    })
+
 })
-
 
 //     $("#").on("submit", function (event) {
 //         event.preventDefault();
